ce005d4e6b710de164d34611fd86623fac2ae8be
Kaio1505 <kaio1505@gmail.com>
2021-07-21 19:58:10 -0300

    Adicionada nova tela inicial

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
+    public GameObject iniciar;
+    public float timer1;
+    public float timer2;
+    float oldTimer;
+    bool isRunning = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer1 = 1;
+        timer2 = 0.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (isRunning)
+        {
+            timer1 -= Time.deltaTime;
+            if (timer1 < 0)
+            {
+                isRunning = false;
+                timer2 = 0.5f;
+                iniciar.SetActive(false);
+            }          
+        }
+        else
+        {
+            timer2 -= Time.deltaTime;
+            if (timer2 < 0)
+            {
+                isRunning = true;
+                timer1 = 1;
+                iniciar.SetActive(true);
+            }
+        }
+        if (Input.GetMouseButtonDown(0))
         {
             SceneManager.LoadScene("CellInvaders");
         }
