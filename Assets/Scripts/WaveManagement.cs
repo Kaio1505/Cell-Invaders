c309c9fb64fd7a83b92cb449761eaf914125ad77
Kaio1505 <kaio1505@gmail.com>
2021-07-21 22:27:19 -0300

    Criação de tela de instrução e tela de vitória

--- Assets/Scripts/WaveManagement.cs
+++ Assets/Scripts/WaveManagement.cs
@@ -1,25 +1,65 @@
 using UnityEngine;
-
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class WaveManagement : MonoBehaviour
 {
     public Player playerPrefab;
     public AbstractInimigo inimigoPrefab;
     public int numeroDeInimigos;
     public int intervaloEntreInimigos;
+    public GameObject sprite_inimigo;
+    GameObject tela_instrucao;
+    public Text PrefabTexto;
+    public Canvas Pai;
+    AbstractInimigo[] inimigos_totais;
+    int carregou;
 
-    // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
-        
-        var player = Instantiate(playerPrefab, new Vector3(0,0,0), Quaternion.identity);
-        for(int i = 0; i < numeroDeInimigos; i++)
+        carregou = 0;
+        //Criando tela de instrução para usuário
+        tela_instrucao = Instantiate(sprite_inimigo, new Vector3(0, 0.4f, 0), Quaternion.identity);
+        tela_instrucao.transform.position.Set(0, 0.4f, 0);
+        var Texto = Instantiate(PrefabTexto, new Vector3(0, -80, 0), Quaternion.identity);
+        Texto.transform.SetParent(Pai.transform, false);
+        Texto.text = "Este inimigo é o seu primeiro desafio, ele irá te seguir até chegar em você, caso encoste você perde uma vida, porém ele é destruido também" +
+            "Atire nele para tentar destruí-lo antes que ele se aproxime";
+        yield return new WaitForSeconds(6f);
+        Destroy(tela_instrucao);
+        Destroy(Texto);
+
+        //Spawanando inimigos
+        inimigos_totais = new AbstractInimigo[numeroDeInimigos];
+        var player = Instantiate(playerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        for (int i = 0; i < numeroDeInimigos; i++)
         {
-            var inimigo = Instantiate(inimigoPrefab, GetPosition(), Quaternion.identity);
-            inimigo.player = player;
-            inimigo.startTime = i*intervaloEntreInimigos;
+            inimigos_totais[i] = Instantiate(inimigoPrefab, GetPosition(), Quaternion.identity);
+            inimigos_totais[i].player = player;
+            inimigos_totais[i].startTime = i * intervaloEntreInimigos;
         }
+        carregou = 1;
     }
 
+    
+    private void Update()
+    {
+        if(carregou==1 && isNull(inimigos_totais) )
+        {
+            SceneManager.LoadScene("Winner");
+        }
+    }
+    public bool isNull(AbstractInimigo[] inimigos_totais)
+    {
+        for (int i = 0; i < numeroDeInimigos; i++)
+        {
+            if (inimigos_totais[i] != null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
     Vector3 GetPosition()
     {
         var x = Random.Range(0, 10)%2 == 0 ? 1 : -1;
