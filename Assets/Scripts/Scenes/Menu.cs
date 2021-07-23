622d70de04e2794172e9c3540eb8553b4c127512
Kaio1505 <kaio1505@gmail.com>
2021-07-23 19:58:06 -0300

    Adicionada nova tela de Win e tela menu com inimigos

--- Assets/Scripts/Scenes/Menu.cs
+++ Assets/Scripts/Scenes/Menu.cs
@@ -14,15 +14,27 @@ public class Menu : MonoBehaviour
     public Canvas Pai;
     public bool subiu=false;
     Text logo;
-
+    public GameObject[] inimigos;
+    float height;
+    float width;
+    public Camera camera_dimensao;
     void Start()
     {
+        //pega a altura e largura da camera
+        height = 2f * camera_dimensao.orthographicSize;
+        width = height * camera_dimensao.aspect;
+        //Cria as sprites que apareceram de plano de fundo
+        criarSprites();
+
+        //Instancia o texto "Cell Invaders" que irá subir
         logo = Instantiate(PrefabText, new Vector3(10, -300, 0), Quaternion.identity);
         logo.transform.SetParent(Pai.transform, false);
         logo.font = contagion;
         logo.fontSize = 50;
         logo.resizeTextForBestFit = true;
         logo.text = "Cell Invaders";
+
+        //Instancia o texto para poder jogar
         iniciar = Instantiate(PrefabText, new Vector3(10, -126, 0), Quaternion.identity);
         iniciar.transform.SetParent(Pai.transform, false);
         iniciar.font = contagion;
@@ -33,6 +45,31 @@ public class Menu : MonoBehaviour
         isRunning = true;
     }
 
+    public void criarSprites()
+    {
+        for(int i=0;i<inimigos.Length;i++)
+        {
+            inimigos[i]=Instantiate(inimigos[i], new Vector3((Screen.width/2), (Screen.height/2), 0),Quaternion.identity);
+            Rigidbody2D rb = inimigos[i].GetComponent<Rigidbody2D>();
+            rb.velocity = new Vector3(Random.Range(-10,10), Random.Range(-10, 10),0)*4;
+            inimigos[i].transform.SetParent(Pai.transform, true);
+        }
+    }
+    public void movimentarSprite()
+    {
+        for (int i = 0; i < inimigos.Length; i++)
+        {
+            Rigidbody2D rb = inimigos[i].GetComponent<Rigidbody2D>();
+            if(inimigos[i].transform.position.x> Screen.width/2 || inimigos[i].transform.position.x <0)
+            {
+                rb.velocity.Set(-rb.velocity.x, rb.velocity.y);
+            }
+            if(inimigos[i].transform.position.y> Screen.height / 2 || inimigos[i].transform.position.y<0)
+            {
+                rb.velocity.Set(rb.velocity.x, -rb.velocity.y);
+            }
+        }
+    }
     public void descerLogo()
     {
         var translaction = 20 * Time.deltaTime;
@@ -46,7 +83,8 @@ public class Menu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!subiu)
+        movimentarSprite();
+        if (!subiu)
         {
             descerLogo();
         }
